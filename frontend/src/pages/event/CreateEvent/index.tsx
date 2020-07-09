ea7deb851d80166c91a7190902cd1966dbfb4497
kaelsilva <kaelsilvaaraujo@gmail.com>
2020-07-08 21:40:56 -0300

    Adicionar página de criar evento. #65

--- frontend/src/pages/event/CreateEvent/index.tsx
+++ frontend/src/pages/event/CreateEvent/index.tsx
@@ -1,9 +1,66 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './styles.css';
 
 const CreateEvent = () => {
+  function handleSubmit() {
+    alert('função handleSubmit executada');
+  }
+
   return (
-    <div>
-      <h1>Hello</h1>
+    <div id="page-create-event">
+      <header>
+        <Link to="/">
+          Voltar para home
+        </Link>
+        <h1>Criar evento</h1>
+      </header>
+
+      <form onSubmit={()=>handleSubmit()}>
+        <fieldset>
+          <legend>
+            <h2>Dados</h2>
+          </legend>
+
+          <div className="field">
+            <label htmlFor="name">Nome do evento</label>
+            <input 
+              type="text" 
+              name="name" 
+              id="name" 
+              onChange={()=>{}}
+            />
+          </div>
+
+          <div className="field">
+            <label htmlFor="date">Data inicial do evento</label>
+            <input 
+              type="text" 
+              name="date" 
+              id="date" 
+              onChange={()=>{}}
+            />
+          </div>
+
+          <div className="field">
+            <label htmlFor="hour">Hora inicial do evento</label>
+            <input 
+              type="text" 
+              name="hour" 
+              id="hour" 
+              onChange={()=>{}}
+            />
+          </div>
+
+        </fieldset>
+
+        <div>
+        <button type="submit">
+          Criar
+        </button>
+        </div>
+      </form>
     </div>
   );
 }
