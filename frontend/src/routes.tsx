88f74c837bd5d1ecc19b00124e63af93361ca00e
kaelsilva <kaelsilvaaraujo@gmail.com>
2020-07-08 21:39:57 -0300

    Adicionar rota para a página Home. #65

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { Route, BrowserRouter } from 'react-router-dom';
 
 import CreateEvent from './pages/event/CreateEvent';
+import Home from './pages/Home';
 
 const Routes = () => {
   return (
     <BrowserRouter>
-      <Route component={CreateEvent} path="/create-event" exact />
+      <Route component={Home} path="/" exact />
+      <Route component={CreateEvent} path="/create-event" />
     </BrowserRouter>
   );
 }
