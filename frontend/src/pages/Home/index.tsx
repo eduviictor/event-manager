4648d92ede82414164e239ebdd05362009673db8
kaelsilva <kaelsilvaaraujo@gmail.com>
2020-07-09 18:49:54 -0300

    Adicionar botão de alterar evento à página Home. #65

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -4,9 +4,16 @@ import { Link } from 'react-router-dom';
 const Home = () => {
   return (
     <>
-      <Link to="/create-event">
-        Criar evento
-      </Link>
+      <div>
+        <Link to="/create-event">
+          Criar evento
+        </Link>
+      </div>
+      <div>
+        <Link to="/update-event">
+          Alterar evento
+        </Link>
+      </div>
     </>
   );
 }
