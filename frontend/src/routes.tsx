31ec6afdf2aa442fb76daf0acef52e65e3dd903b
arthurmdros <arthurmedeiros2005@gmail.com>
2020-07-22 01:50:48 -0300

    Adicionando rota para registro de clientes

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -5,6 +5,7 @@ import CreateEvent from './pages/event/CreateEvent';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import RegisterOrg from './pages/RegisterOrg';
+import RegisterClt from './pages/RegisterClient';
 import UpdateEvent from './pages/event/UpdateEvent';
 
 const Routes = () => {
@@ -13,7 +14,8 @@ const Routes = () => {
       <Switch>
           <Route component = {Login} path="/" exact  />                
           <Route component={Home} path="/home"/>
-          <Route component={RegisterOrg} path="/registerorganizador"/>
+          <Route component={RegisterOrg} path="/register-organizer"/>
+          <Route component={RegisterClt} path="/register-client"/>
           <Route component={CreateEvent} path="/create-event" />
           <Route component={UpdateEvent} path="/update-event" />
       </Switch>
