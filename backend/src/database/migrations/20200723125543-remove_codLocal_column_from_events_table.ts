cd220be79906d749aafb9287192655d9d230e483
kaelsilva <kaelsilvaaraujo@gmail.com>
2020-07-23 11:23:19 -0300

    Atualizar tabela de eventos na base de dados. #105

---
+++ backend/src/database/migrations/20200723125543-remove_codLocal_column_from_events_table.ts
@@ -0,0 +1,9 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.removeColumn('events', 'codLocal');
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.addColumn('events', 'codLocal', Sequelize.INTEGER);
+  },
+};
