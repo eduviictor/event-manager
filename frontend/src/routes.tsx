299b897217b7e34fd2dc874e0a2b0cb64acfc105
eduviictor <eduvictornobrega@gmail.com>
2020-07-23 10:35:29 -0300

    adicionar login e logout no frontend

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,26 +1,40 @@
 import React from 'react';
-import { Route, BrowserRouter, Switch } from 'react-router-dom';
+import { Route, BrowserRouter, Switch, Redirect } from 'react-router-dom';
 
-import CreateEvent from './pages/event/CreateEvent';
+import CreateEvent from './pages/Event/CreateEvent';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import RegisterOrg from './pages/RegisterOrg';
 import RegisterClt from './pages/RegisterClient';
-import UpdateEvent from './pages/event/UpdateEvent';
+import UpdateEvent from './pages/Event/UpdateEvent';
+import { isAuthenticated } from './services/auth';
+
+const PrivateRoute = ({ component: Component, ...rest }: any) => (
+  <Route
+    {...rest}
+    render={props =>
+      isAuthenticated() ? (
+        <Component {...props} />
+      ) : (
+        <Redirect to={{ pathname: '/', state: { from: props.location } }} />
+      )
+    }
+  />
+);
 
 const Routes = () => {
   return (
     <BrowserRouter>
       <Switch>
-          <Route component = {Login} path="/" exact  />                
-          <Route component={Home} path="/home"/>
-          <Route component={RegisterOrg} path="/register-organizer"/>
-          <Route component={RegisterClt} path="/register-client"/>
-          <Route component={CreateEvent} path="/create-event" />
-          <Route component={UpdateEvent} path="/update-event" />
+        <Route component={Login} path="/" exact />
+        <PrivateRoute component={Home} path="/home" />
+        <PrivateRoute component={RegisterOrg} path="/register-organizer" />
+        <PrivateRoute component={RegisterClt} path="/register-client" />
+        <PrivateRoute component={CreateEvent} path="/create-event" />
+        <PrivateRoute component={UpdateEvent} path="/update-event" />
       </Switch>
     </BrowserRouter>
   );
-}
+};
 
 export default Routes;
