a7a179b434896de1b5c027d00e1e3adf0a20adfa
arthurmdros <arthurmedeiros2005@gmail.com>
2020-07-24 12:47:29 -0300

    Adicionando implementação do atualizar organizador

--- frontend/src/pages/UpdateOrganizer/index.tsx
+++ frontend/src/pages/UpdateOrganizer/index.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect, FormEvent} from 'react';
 import { FiArrowLeft } from 'react-icons/fi';
 import { Link, useHistory} from 'react-router-dom';
 
@@ -9,8 +9,9 @@ import './styles.css';
 
 const OrganizerUpdate = () => {
     
-    const navigation = useHistory();
+    const cpnj = localStorage.getItem('org_id')
 
+    const history = useHistory();
     
     const [cnpj, setCnpj] = useState('');
     const [nome, setNome] = useState('');
@@ -19,9 +20,51 @@ const OrganizerUpdate = () => {
     const [senha, setSenha] = useState('');
     const [telefone, setTelefone] = useState('');
 
+    
+    useEffect(() => {
+        api.get(`/organizers/${cnpj}`).then(
+            res => {
+                setCnpj(res.data[0].cnpj);
+                setNome(res.data[0].nome);
+                setEmail(res.data[0].email);
+                setLogin(res.data[0].login);
+                setTelefone(res.data[0].telefone);
+            }
+        )
+    }, []);        
+
    
-    async function updateOrganizer(){
-        alert('atualizar');
+    async function updateOrganizer(event: FormEvent){
+        event.preventDefault();
+
+        const data = {
+            cnpj,
+            nome,
+            email,
+            login,
+            senha,
+            telefone
+        };  
+
+         if(data.cnpj === '' || data.nome === '' || data.email === '' || data.login === '' || data.senha === '' || data.telefone === ''){
+            alert('Erro! Verifique se os campos preenchidos estão corretos.');
+            return;
+        }else{
+
+            try {
+                const response = await api.put(`/organizers/${cnpj}`, data);        
+                if (response.status != 200){
+                alert(`Erro ${response.status}.`);
+                }
+            } catch (err){
+                return;
+            }
+
+                
+            alert('Cliente atualizado com sucesso!');
+            
+            history.push('/home');   
+        }
     }
 
     return(
@@ -39,6 +82,7 @@ const OrganizerUpdate = () => {
                 <form onSubmit={updateOrganizer}>
                 <h1>CNPJ:</h1>
                 <input
+                    value= {cnpj}
                     name="cnpj"
                     type="text"
                     id="cnpj"
@@ -46,6 +90,7 @@ const OrganizerUpdate = () => {
                 />
                 <h1>Nome:</h1>
                 <input
+                    value= {nome}
                     name="nome"
                     type="text"
                     id="nome"
@@ -53,6 +98,7 @@ const OrganizerUpdate = () => {
                 />
                 <h1>E-Mail:</h1>
                 <input
+                    value= {email}
                     name="email"
                     type="text"
                     id="email"
@@ -60,6 +106,7 @@ const OrganizerUpdate = () => {
                 />
                 <h1>Usuário:</h1>
                 <input
+                    value= {login}
                     name="login"
                     type="text"
                     id="login"
@@ -74,6 +121,7 @@ const OrganizerUpdate = () => {
                     />
                 <h1>Telefone:</h1>
                 <input
+                    value= {telefone}
                     name="telefone"
                     type="text"
                     id="telefone"
