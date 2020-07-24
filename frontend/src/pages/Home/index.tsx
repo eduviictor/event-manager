ed76a67f2469b1446c8e51624857efefd4bf192f
kaelsilva <kaelsilvaaraujo@gmail.com>
2020-07-24 09:30:04 -0300

    Adicionar exibição de eventos na página home. #105

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BsPower, BsTrash } from 'react-icons/bs';
 import { FiArrowRight } from 'react-icons/fi';
 import { Link, useHistory } from 'react-router-dom';
@@ -6,6 +6,8 @@ import { Link, useHistory } from 'react-router-dom';
 import logo from '../../assets/logo.png';
 import './styles.css';
 import { logout } from '../../services/auth';
+
+import api from '../../services/api';
 
 const Home = () => {
   const navigation = useHistory();
@@ -15,15 +17,45 @@ const Home = () => {
     navigation.push('/');
   }
 
+  interface EventAttributes {
+    name: string;
+    description: string;
+  }
+
+  const [eventsData, setEventsData] = useState({
+    events: []
+  });
+
+  useEffect(()=>{
+    const getData = async () => {
+      await api.get('/events').then(response => {
+        const events = response.data;
+        setEventsData({ events: events });
+      });
+    }
+    getData();
+  },[]);
+
+  function handleDelete(key:string) {
+    const codigo = Number(key);
+    const response = async () => {
+      await api.delete(`/events/${codigo}`);
+      alert('Evento deletado com sucesso!');
+      navigation.go(0);
+      return;
+    };
+    response();
+  }
+
   return (
     <div className="homeContainer">
       <header>
         <img src={logo} alt="Event Manager" />
+
         <span>Bem vindo, usuário</span>
+        
         <div>
           <Link to="/create-event">Criar evento</Link>
-
-          <Link to="/update-event">Alterar evento</Link>
         </div>
         <button onClick={handleLogout} type="button">
           <BsPower size={18} color="#E02041" />
@@ -32,31 +64,35 @@ const Home = () => {
 
       <h1>Eventos</h1>
 
-      <ul>
-        <li>
-          <strong>Evento:</strong>
-          <p>nome do evento</p>
+      {eventsData.events.map((event: any) => (
+        <div key={event.codigo}>
+          <ul>
+            <li>
+              <strong>Evento:</strong>
+              <p>{event.nome}</p>
 
-          <strong>Tipo evento:</strong>
-          <p>informações do evento</p>
+              <strong>Tipo evento:</strong>
+              <p>{event.description}</p>
 
-          <button
-            className="button-delete"
-            onClick={() => alert('Deletar evento')}
-            type="button"
-          >
-            <BsTrash size={18} color="#1393f6" />
-          </button>
-          <button
-            className="detail-link"
-            onClick={() => alert('Visualizar detalhes')}
-            type="button"
-          >
-            Ver detalhes
-            <FiArrowRight size={16} color="#1393f6" />
-          </button>
-        </li>
-      </ul>
+              <button
+                className="button-delete"
+                onClick={() => handleDelete(event.codigo)}
+                type="button"
+              >
+                <BsTrash size={18} color="#1393f6" />
+              </button>
+              <Link 
+                to={`/update-event/${event.codigo}`} 
+                className="detail-link" 
+                type="button"
+              >
+                Configurações
+                <FiArrowRight size={16} color="#1393f6" />
+              </Link>
+            </li>
+          </ul>
+        </div>
+      ))}
     </div>
   );
 };
