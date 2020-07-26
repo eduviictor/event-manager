3c8d031806f5494137efb9fbf1667b8dd21b0a71
kaelsilva <kaelsilvaaraujo@gmail.com>
2020-07-26 15:50:06 -0300

    Add rotas de ingresso. #105

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, BrowserRouter, Switch, Redirect } from 'react-router-dom';
 
-import CreateEvent from './pages/event/CreateEvent';
+import CreateEvent from './pages/Event/CreateEvent';
 import Home from './pages/Home';
 import HomeClient from './pages/HomeClient';
 import Login from './pages/Login';
@@ -9,9 +9,11 @@ import RegisterOrg from './pages/RegisterOrg';
 import RegisterClt from './pages/RegisterClient';
 import ClientUpdate from './pages/UpdateClient';
 import OrganizerUpdate from './pages/UpdateOrganizer';
-import UpdateEvent from './pages/event/UpdateEvent';
+import UpdateEvent from './pages/Event/UpdateEvent';
 import CreateAttraction from './pages/attraction/CreateAttraction';
 import UpdateAttraction from './pages/attraction/UpdateAttraction';
+import CreateTicket from './pages/Ticket/CreateTicket';
+import UpdateTicket from './pages/Ticket/UpdateTicket';
 import { isAuthenticated } from './services/auth';
 
 const PrivateRoute = ({ component: Component, ...rest }: any) => (
@@ -39,9 +41,11 @@ const Routes = () => {
         <PrivateRoute component={ClientUpdate} path="/update-client" />   
         <PrivateRoute component={OrganizerUpdate} path="/update-organizer" />           
         <PrivateRoute component={CreateEvent} path="/create-event" />        
-        <PrivateRoute component={UpdateEvent} path="/update-event" />        
+        <PrivateRoute component={UpdateEvent} path="/update-event/:codigo" />        
         <PrivateRoute component={CreateAttraction} path="/create-attraction" />
         <PrivateRoute component={UpdateAttraction} path="/update-attraction" />
+        <PrivateRoute component={CreateTicket} path="/create-ticket/:cod_evento" />
+        <PrivateRoute component={UpdateTicket} path="/update-ticket/:codigo" />
       </Switch>
     </BrowserRouter>
   );
