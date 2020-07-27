369824c97246b53a8ed33a2ade3374ff879507f1
eduviictor <eduvictornobrega@gmail.com>
2020-07-27 09:45:42 -0300

    Alterar nome da pasta de evento do frontend

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, BrowserRouter, Switch, Redirect } from 'react-router-dom';
 
-import CreateEvent from './pages/event/CreateEvent';
+import CreateEvent from './pages/Event/CreateEvent';
 import Home from './pages/Home';
 import HomeClient from './pages/HomeClient';
 import Login from './pages/Login';
@@ -9,7 +9,7 @@ import RegisterOrg from './pages/RegisterOrg';
 import RegisterClt from './pages/RegisterClient';
 import ClientUpdate from './pages/UpdateClient';
 import OrganizerUpdate from './pages/UpdateOrganizer';
-import UpdateEvent from './pages/event/UpdateEvent';
+import UpdateEvent from './pages/Event/UpdateEvent';
 import CreateAttraction from './pages/attraction/CreateAttraction';
 import UpdateAttraction from './pages/attraction/UpdateAttraction';
 import { isAuthenticated } from './services/auth';
@@ -33,13 +33,13 @@ const Routes = () => {
       <Switch>
         <Route component={Login} path="/" exact />
         <PrivateRoute component={Home} path="/home" />
-        <PrivateRoute component={HomeClient} path="/home-client" />        
+        <PrivateRoute component={HomeClient} path="/home-client" />
         <Route component={RegisterOrg} path="/register-organizer" />
         <Route component={RegisterClt} path="/register-client" />
-        <PrivateRoute component={ClientUpdate} path="/update-client" />   
-        <PrivateRoute component={OrganizerUpdate} path="/update-organizer" />           
-        <PrivateRoute component={CreateEvent} path="/create-event" />        
-        <PrivateRoute component={UpdateEvent} path="/update-event" />        
+        <PrivateRoute component={ClientUpdate} path="/update-client" />
+        <PrivateRoute component={OrganizerUpdate} path="/update-organizer" />
+        <PrivateRoute component={CreateEvent} path="/create-event" />
+        <PrivateRoute component={UpdateEvent} path="/update-event" />
         <PrivateRoute component={CreateAttraction} path="/create-attraction" />
         <PrivateRoute component={UpdateAttraction} path="/update-attraction" />
       </Switch>
